8ec48e6cff397030d8ad855096ac1f6683d1d9bb
Paul Swartz <paul@paulswartz.net>
2023-05-10 10:19:17 -0400

    wip: try resolving the remote schema

--- src/components/EventSchema.tsx
+++ src/components/EventSchema.tsx
@@ -4,21 +4,47 @@ import deepmerge from "deepmerge";
 // @ts-ignore
 import JSONSchemaViewer from "@theme/JSONSchemaViewer";
 
+const resolverOptions = {
+  resolvers: {
+    https: {
+      resolve: (ref: string) => {
+        return new Promise((resolve, reject) => {
+          fetch(ref.toString(), {
+            headers: {
+              Accept: "application/json",
+            },
+          })
+            .then((response) => response.json())
+            .then((response) => {
+              console.log(response);
+              return response;
+            })
+            .then((result) => resolve(result))
+            .catch((err) => reject(err));
+        });
+      },
+    },
+  },
+  transformDereferenceResult: (result) => {
+    // merge the resolved schema back with the original, so that the descriptions/examples aren't thrown away
+    const merged = deepmerge(props.schema, result.result, {
+      arrayMerge: (_, source) => source,
+    });
+    return Promise.resolve({ result: merged });
+  },
+};
+
+const viewerOptions = {
+  showExamples: true,
+};
+
 export default function EventSchema(props: { schema: object }): JSX.Element {
   return (
     <div>
       <JSONSchemaViewer
         schema={props.schema}
-        resolverOptions={{
-          transformDereferenceResult: (result) => {
-            // merge the resolved schema back with the original, so that the descriptions/examples aren't thrown away
-            const merged = deepmerge(props.schema, result.result, {
-              arrayMerge: (_, source) => source,
-            });
-            return Promise.resolve({ result: merged });
-          },
-        }}
-        viewerOptions={{ showExamples: true }}
+        resolverOptions={resolverOptions}
+        viewerOptions={viewerOptions}
       />
       JSON Schema :
       <CodeBlock language="json">
